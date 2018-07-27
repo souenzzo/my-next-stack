cb86a437cc5c8c97d18a21b4fe059357e2834a39
Enzzo Cavallo <souenzzo@gmail.com>
2018-07-27 01:26:01 +0000

    +specs

--- src/server/telegram.clj
+++ src/server/telegram.clj
@@ -1,6 +1,7 @@
 (ns server.telegram
   (:require [clojure.data.json :as json]
-            [clojure.string :as string])
+            [clojure.string :as string]
+            [clojure.spec.alpha :as s])
   (:import (clojure.lang Keyword)
            (java.net URLEncoder)
            (java.nio.charset StandardCharsets)))
@@ -19,12 +20,27 @@
   (encode [s]
     (encode (name s))))
 
+(s/fdef encode
+        :args (s/cat :s (s/or :string string?
+                              :number number?
+                              :keyword keyword?))
+        :ret string?)
+
 (defn args->query
   [args]
   (if (empty? args)
     ""
     (string/join "&" (for [[k v] args]
                        (string/join "=" (map encode [k v]))))))
+
+(s/def ::query-args
+  (s/nilable (s/map-of keyword? (s/or :string string?
+                                      :number number?))))
+
+(s/fdef args->query
+        :args (s/cat :args ::query-args)
+        :ret string?)
+
 
 (defn method-uri
   ([token method] (method-uri token method nil))
@@ -33,6 +49,12 @@
      (if (empty? args)
        uri
        (format "%s?%s" uri (args->query args))))))
+
+(s/fdef method-uri
+        :args (s/cat :token string?
+                     :method keyword?
+                     :args (s/? ::query-args))
+        :ret string?)
 
 (defn request!
   [uri]
@@ -49,12 +71,38 @@
               (map (juxt :username :id)))
         updates))
 
+(s/fdef updates->user-index
+        :args (s/cat :updates ::updates)
+        :fn (fn [{:keys [args ret]}] (>= (count (:updates args))
+                                         (count ret)))
+        :ret (s/map-of ::username ::id))
+
 (defn send-two-factor
   [token username text updates]
   (let [username->chat-id (updates->user-index updates)
         chat-id (username->chat-id username)]
     (when chat-id
       (method-uri token :sendMessage {:chat_id chat-id :text text}))))
+
+(s/def ::is_bot boolean?)
+(s/def ::username string?)
+(s/def ::id number?)
+(s/def ::from
+  (s/keys :req-un [::username ::id]
+          :opt-un [::is_bot]))
+(s/def ::message
+  (s/keys :req-un [::from]))
+(s/def ::update
+  (s/keys :req-un [::message]))
+(s/def ::updates
+  (s/coll-of ::update))
+
+(s/fdef send-two-factor
+        :args (s/cat :token string?
+                     :username string?
+                     :text string?
+                     :updates ::updates)
+        :ret (s/nilable string?))
 
 
 (defn send-two-factor!
