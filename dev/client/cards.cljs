ebe3f89b28131a06663223067bcb3514d0f5e882
Enzzo Cavallo <souenzzo@gmail.com>
2018-08-07 01:39:03 +0000

    +tic tac toe

--- dev/client/cards.cljs
+++ dev/client/cards.cljs
@@ -21,6 +21,86 @@
   (r/atom {:text "Hello!"})
   {:inspect-data true})
 
+(defn box
+  [{:keys [shape x y on-click]}]
+  (let [+x #(+ % (* 10 x))
+        +y #(+ % (* 10 y))]
+    [:<>
+     [:rect {:x        (+x 1)
+             :y        (+y 1)
+             :style    {:fill-opacity ".1"}
+             :on-click on-click
+             :width    8
+             :height   8}]
+     (case shape
+       :circle [:circle {:r            4
+                         :cx           (+x 5)
+                         :cy           (+y 5)
+                         :stroke-width 1
+                         :stroke       :black
+                         :fill         :none}]
+       :cross [:<>
+               [:line {:x1     (+x 9)
+                       :x2     (+x 1)
+                       :y1     (+y 9)
+                       :y2     (+y 1)
+                       :stroke :black}]
+               [:line {:x1     (+x 9)
+                       :x2     (+x 1)
+                       :y1     (+y 1)
+                       :y2     (+y 9)
+                       :stroke :black}]]
+       nil)]))
+
+(defn grid
+  []
+  [:<>
+   [:line {:x1     10
+           :x2     10
+           :y1     0
+           :y2     30
+           :stroke :black}]
+   [:line {:x1     20
+           :x2     20
+           :y1     0
+           :y2     30
+           :stroke :black}]
+   [:line {:x1     0
+           :x2     30
+           :y1     10
+           :y2     10
+           :stroke :black}]
+   [:line {:x1     0
+           :x2     30
+           :y1     20
+           :y2     20
+           :stroke :black}]])
+
+(def empty-state
+  {:shape :circle
+   :table (vec (repeat 3 (vec (repeat 3 :_))))})
+
+
+(dc/defcard tic-tac-toe
+  (dc/reagent (fn [state owner]
+                (let [{:keys [shape x y table]} @state]
+                  [:div
+                   [:button {:on-click #(reset! state empty-state)} "cleanup"]
+                   [:svg {:view-box "0 0 100 100"}
+                    (for [i (range 3)
+                          j (range 3)]
+                      [box {:key      (str [i j])
+                            :on-click (fn []
+                                        (swap! state (comp
+                                                       #(update % :table assoc-in [i j] shape)
+                                                       #(update % :shape {:circle :cross :cross :circle}))))
+                            :x        i
+                            :y        j
+                            :shape    (get-in table [i j])}])]])))
+  (r/atom empty-state)
+  {:inspect-data true})
+
+
 (dc/defcard auth
   (dc/reagent (fn [state owner]
                 (let [{:keys [username two-factor-in-progress? two-factor-text authed?]} @state]
