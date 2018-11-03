4e6cde908f3a1876a98ac3a4986d10c90a9b8c68
Enzzo Cavallo <souenzzo@gmail.com>
2018-11-03 13:19:00 -0300

    WIP

--- test/server/walkable_test.clj
+++ test/server/walkable_test.clj
@@ -2,6 +2,7 @@
   (:require [clojure.test :refer [deftest use-fixtures]]
             [midje.sweet :refer :all]
             [walkable.sql-query-builder :as sqb]
+            [walkable.sql-query-builder.pathom-env :as penv]
             [clojure.java.jdbc :as j]
             [clojure.java.io :as io]
             [com.wsscode.pathom.connect :as pc]
@@ -31,7 +32,8 @@
      :joins          {:account/friends [:account/id :friends/account_id
                                         :friends/friend_id :account/id]}
      :reversed-joins {}
-     :idents         {:account/by-slug :account/slug}
+     :idents         {:account/by-slug :account/slug
+                      :account/slug    :account/slug}
      :quote-marks    sqb/quotation-marks}))
 
 (def parser-w
@@ -44,33 +46,35 @@
 
 
 
-;; How to go from :person/id to that person's details
-(pc/defresolver person-resolver
-  [env {:keys [person/id] :as params}]
-  ;; The minimum data we must already know in order to resolve the outputs
-  {::pc/input  #{:person/id}
-   ;; A query template for what this resolver outputs
-   ::pc/output [:person/name {:person/address [:address/id]}]}
-  ;; normally you'd pull the person from the db, and satisfy the listed
-  ;; outputs. For demo, we just always return the same person details.
-  {:person/name    "Tom"
-   :person/address {:address/id 1}})
+(def person-resolver
+  (pc/resolver `person-resolver
+               {::pc/input  #{:account/slug}
+                ::pc/output [:account/name
+                             :account/slug
+                             {:account/friends [:account/id]}]}
+               (fn [{::sqb/keys [run-query sql-schema sql-db]
+                     ::p/keys   [parent-query]
+                     :as        env} {:keys [account/by-slug]
+                                      :as   params}]
 
-;; how to go from :address/id to address details.
-(pc/defresolver address-resolver
-  [env {:keys [address/id] :as params}]
-  {::pc/input  #{:address/id}
-   ::pc/output [:address/city :address/state]}
-  {:address/city  "Salem"
-   :address/state "MA"})
+                 (clojure.pprint/pprint (penv/dispatch-key env))
+                 (clojure.pprint/pprint (::sqb/target-tables sql-schema))
+                 (clojure.pprint/pprint (sqb/pull-entities env))
+                 (first (run-query sql-db ["SELECT  \"account\".\"name\" AS \"account/name\"  FROM \"account\" WHERE (\"account\".\"slug\")=( ? )" by-slug])))))
 
 ;; define a list with our resolvers
-(def my-resolvers [person-resolver address-resolver])
+(def my-resolvers [person-resolver])
 
 ;; setup for a given connect system
 (def parser-c
   (p/parser
-    {::p/env     {::p/reader [p/map-reader pc/reader]}
+    {::p/env     {::p/reader       [p/map-reader
+                                    pc/all-readers]
+                  ::sqb/sql-db     db
+                  ::sqb/sql-schema compiled-schema
+                  ::sqb/run-query  (fn [& args]
+                                     (prn (second args))
+                                     (apply j/query args))}
      ::p/plugins [(pc/connect-plugin {::pc/register my-resolvers})]}))
 
 (deftest my-sql-test
@@ -89,6 +93,7 @@
                                         {:friend/account_id ian-id
                                          :friend/friend_id  enzzo-id}])]
     (fact
+      "Original"
       (parser-w {} [{[:account/by-slug "enzzo"] [:account/name]}])
       => {[:account/by-slug "enzzo"] {:account/name "Enzzo"}})
     (fact
@@ -98,11 +103,13 @@
                                                         {:account/name "Ingrid"}]
                                       :account/name    "Enzzo"}})
     (fact
-      (parser-c {} [{[:account/by-slug "enzzo"] [:account/name]}])
-      => {[:account/by-slug "enzzo"] {:account/name "Enzzo"}})
-    (fact
-      (parser-c {} [{[:account/by-slug "enzzo"] [:account/name
-                                                 {:account/friends [:account/name]}]}])
-      => {[:account/by-slug "enzzo"] {:account/friends [{:account/name "Ian"}
-                                                        {:account/name "Ingrid"}]
-                                      :account/name    "Enzzo"}})))
+      "walkable connect"
+      (parser-c {} [{[:account/slug "enzzo"] [:account/slug
+                                              :account/name]}])
+      => {[:account/slug "enzzo"] {:account/name "Enzzo"}})
+    #_(fact
+        (parser-c {} [{[:account/by-slug "enzzo"] [:account/name
+                                                   {:account/friends [:account/name]}]}])
+        => {[:account/by-slug "enzzo"] {:account/friends [{:account/name "Ian"}
+                                                          {:account/name "Ingrid"}]
+                                        :account/name    "Enzzo"}})))
