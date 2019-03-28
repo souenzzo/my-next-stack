280955ca9315b2feaaa92d5a698c890ec14f817d
Enzzo Cavallo <souenzzo@gmail.com>
2019-03-28 00:07:57 -0300

    New schema WIP

--- resources/schema.sql
+++ resources/schema.sql
@@ -1,21 +1,28 @@
-CREATE TABLE account (
+CREATE TABLE app_user (
     id SERIAL UNIQUE NOT NULL PRIMARY KEY,
     username TEXT UNIQUE NOT NULL
 );
 
-CREATE TABLE chat (
+CREATE TABLE app_session (
+    id SERIAL UNIQUE NOT NULL PRIMARY KEY,
+    csrf TEXT UNIQUE NOT NULL,
+    authed SERIAL references app_user(id)
+)
+
+
+CREATE TABLE app_chat (
     id SERIAL UNIQUE NOT NULL PRIMARY KEY,
     name TEXT
 );
 
-CREATE TABLE account_chat (
+CREATE TABLE app_user_chat (
     id SERIAL UNIQUE NOT NULL PRIMARY KEY,
-    account SERIAL NOT NULL references account(id),
-    chat SERIAL NOT NULL references chat(id)
+    account SERIAL NOT NULL references app_user(id),
+    chat SERIAL NOT NULL references app_chat(id)
 );
 
-CREATE TABLE message (
+CREATE TABLE app_message (
     id SERIAL UNIQUE NOT NULL PRIMARY KEY,
-    author SERIAL NOT NULL references account(id),
+    author SERIAL NOT NULL references app_user(id),
     body TEXT NOT NULL
 );
