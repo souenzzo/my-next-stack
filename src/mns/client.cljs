cf2ff69a6215678e0416534182d2629a68806c6b
Enzzo Cavallo <souenzzo@gmail.com>
2022-12-02 19:52:04 -0300

    +local dev

--- src/mns/client.cljs
+++ src/mns/client.cljs
@@ -2,8 +2,22 @@
   (:require ["react" :as r]
             ["react-dom/client" :as rdc]))
 
+(defn ui-counter
+  []
+  (let [[n set-n] (r/useState 0)]
+    (r/createElement "div" #js{}
+      (r/createElement "button" #js{:onClick (fn [evt]
+                                               (set-n (dec n)))}
+        "-")
+      (str n)
+      (r/createElement "button" #js{:onClick (fn [evt]
+                                               (set-n (inc n)))}
+        "+"))))
+
 (def ui-root
-  (r/createElement "div" #js{} "Hello World"))
+  (r/createElement "div" #js{}
+    "Hello World"
+    (r/createElement ui-counter)))
 
 (defonce *root
   (atom nil))
--- src/mns/main.clj
+++ src/mns/main.clj
@@ -1,7 +1,35 @@
-(ns mns.main)
+(ns mns.main
+  (:require [io.pedestal.http :as http]
+            [io.pedestal.log :as log]))
+
+(defonce *server
+  (atom nil))
 
 (defn -main
-  [& _])
+  [& _]
+  (swap! *server
+    (fn [server]
+      (some-> server http/stop)
+      (-> {::http/type          :jetty
+           ::http/join?         false
+           ::http/port          8080
+           ::http/routes        #{}
+           ::http/secure-headers nil
+           ::http/file-path     "target/classes/public"
+           ::http/resource-path "public"}
+        http/default-interceptors
+        http/create-server
+        http/start))))
+
+(defn dev-main
+  [& _]
+  (-main)
+  (-> `shadow.cljs.devtools.server/start!
+    requiring-resolve
+    (apply []))
+  (-> `shadow.cljs.devtools.api/watch
+    requiring-resolve
+    (apply [:client])))
 
 (comment
-  (-main))
+  (dev-main))
